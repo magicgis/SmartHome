5d602a9ea4e8775af49a2ea24d5cefd7433b1082
Sebastian Bischlager <sebastian-bischlager@web.de>
2015-12-31 14:53:18 +0100

    Added comments in ESApi

--- Embedded_Python/Core/ESApi/FileIO.py
+++ Embedded_Python/Core/ESApi/FileIO.py
@@ -1,27 +1,56 @@
+"""
+    FileIO provides functions for everything that happens on hard disk
+"""
+
 import os
 
 
 def root_directory() -> str:
+    """
+    :return: Path to the systems root directory
+    """
+
     filePath = os.path.realpath(__file__)
     fileDir = os.path.dirname(filePath)
     rootDir = os.path.join(fileDir, "../..");
     rootDir = os.path.realpath(rootDir)
     return rootDir
 
+
 def apps_directory() -> str:
+    """
+    :return: Path to the directory containing all apps
+    """
+
     return os.path.join(root_directory(), "Apps")
 
 
 def data_directory() -> str:
+    """
+    :return: Path to dir containing system and app data
+    """
+
     return os.path.join(root_directory(), "Data")
 
 
 def sysdata_directory() -> str:
+    """
+    :return: Path to dir containing sytem data
+    """
+
     return os.path.join(data_directory(), "SysData")
 
 
 def appdata_directory() -> str:
+    """
+    :return: Path to dir containing app data
+    """
+
     return os.path.join(data_directory(), "AppData")
 
 def settings_directory() -> str:
+    """
+    :return: Path to dir containing settings
+    """
+
     return os.path.join(sysdata_directory(), "settings")
--- Embedded_Python/Core/ESApi/IXPFile.py
+++ Embedded_Python/Core/ESApi/IXPFile.py
@@ -1,12 +1,19 @@
 from xml.dom import minidom
 
 class IXPFile:
+    """
+        A IXP File is a XML File containing data used for the smart home system.
+        This file structure is used for network communication between clients and the server
+    """
+
+    # Variables containing data that gets parsed to XML or has been parsed from existing source
     __network_function = ""  # type: str
     __header_names = []  # type: List[str]
     __header_values = []  # type: List[str]
     __info_names = []  # type: List[str]
     __info_values = []  # type: List[str]
 
+    # Constants defining the IXP Layout
     __XML_HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
     __XML_IXP = "IXP"
     __XML_IXP_TARGET = "Target"
@@ -30,14 +37,23 @@ class IXPFile:
             "</" + __XML_IXP + ">"
 
     def __init__(self, source = None):
+        """
+            Initialized a new IXP File or parses a existing one provided as string
+        :param source: Existing IXP String to parse
+        """
+
+        # If no source is provided everything is done already
         if source is None:
             return
 
+        # Parse source to minidom xml
         doc = minidom.parseString(source)
 
+        # Read the network function
         nFuncNode = doc.getElementsByTagName(self.__XML_IXP_TARGET)[0]
         self.__network_function = nFuncNode.getAttribute(self.__XML_IXP_TARGET_NFUNCTION)
 
+        # Read headers
         headerNode = doc.getElementsByTagName(self.__XML_IXP_HEADER)[0]
         headerNodes = headerNode.getElementsByTagName(self.__XML_IXP_HEADER_INFO)
 
@@ -47,9 +63,10 @@ class IXPFile:
             self.__header_names.append(name)
             self.__header_values.append(value)
 
+
+        # Read infos
         bodyNode = doc.getElementsByTagName(self.__XML_IXP_BODY)[0]
         bodyNodes = bodyNode.getElementsByTagName(self.__XML_IXP_BODY_INFO)
-
 
         for curNode in bodyNodes:
             name = curNode.getAttribute(self.__XML_IXP_BODY_INFO_NAME)
@@ -58,11 +75,19 @@ class IXPFile:
             self.__info_values.append(value)
 
     def get_xml(self) -> str:
+        """
+            Parses the object to an IXP String
+        :return: Fully parsed string
+        """
+
+        # Create a raw IXP File with minidom xml
         doc = minidom.parseString(self.__XML_BASE)
 
+        # Set network function
         nFuncNode = doc.getElementsByTagName(self.__XML_IXP_TARGET)[0]
         self.__network_function = nFuncNode.setAttribute(self.__XML_IXP_TARGET_NFUNCTION, self.__network_function)
 
+        # Set headers
         for index in range(0, self.headers_count()):
             headerNode = doc.createElement(self.__XML_IXP_HEADER_INFO)
             headerNode.setAttribute(self.__XML_IXP_HEADER_INFO_NAME, self.header_name_at(index))
@@ -70,6 +95,7 @@ class IXPFile:
 
             doc.getElementsByTagName(self.__XML_IXP_HEADER)[0].appendChild(headerNode)
 
+        # Set infos
         for index in range(0, self.infos_count()):
             infoNode = doc.createElement(self.__XML_IXP_BODY_INFO)
             infoNode.setAttribute(self.__XML_IXP_BODY_INFO_NAME, self.info_name_at(index))
@@ -82,42 +108,90 @@ class IXPFile:
         return doc.toxml()
 
     def get_network_function(self) -> str:
+        """
+        :return: The network function
+        """
+
         return self.__network_function
 
     def headers_count(self) -> int:
+        """
+        :return: Number of headers
+        """
+
         return len(self.__header_names)
 
     def header_name_at(self, index: int) -> str:
+        """
+            Provides the name of a header at a specific index
+        :param index: The headers index
+        :return: The headers name
+        """
+
         if index < 0 or index >= self.headers_count():
             raise IndexError()
 
         return self.__header_names[index]
 
     def header_value_at(self, index: int) -> str:
+        """
+            Provides the value of a header at a specific index
+        :param index: The headers index
+        :return: The headers value
+        """
+
         if index < 0 or index >= self.headers_count():
             raise IndexError()
 
         return self.__header_values[index]
 
     def infos_count(self) -> int:
+        """
+        :return: Number of infos
+        """
+
         return len(self.__info_names)
 
     def info_name_at(self, index: int) -> str:
+        """
+            Provides the name of a info at a specific index
+        :param index: The infos index
+        :return: The infos name
+        """
+
         if index <  0 or index >= self.infos_count():
             raise IndexError()
 
         return self.__info_names[index]
 
     def info_value_at(self, index: int) -> str:
+        """
+            Provides the value of a info at a specific index
+        :param index: The infos index
+        :return: The infos value
+        """
+
         if index < 0 or index >= self.infos_count():
             raise IndexError()
 
         return self.__info_values[index]
 
     def add_info(self, name: str, value: str):
+        """
+            Adds a new info to the IXP File
+        :param name: The infos name
+        :param value: The infos value
+        """
+
         self.__info_names.append(name)
         self.__info_values.append(value)
 
     def add_header(self, name: str, value: str):
+        """
+            Provides a new header to the IXP File
+        :param name: The headers name
+        :param value: The headers value
+        """
+
         self.__header_names.append(name)
         self.__header_values.append(value)
