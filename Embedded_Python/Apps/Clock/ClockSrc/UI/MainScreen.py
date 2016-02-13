b1fb4f7b4537db5e335581701167657338cba6b6
Sebastian Bischlager <sebastoan-bischlager@web.de>
2016-02-13 18:28:17 +0100

    Added Date Label

--- Embedded_Python/Apps/Clock/ClockSrc/UI/MainScreen.py
+++ Embedded_Python/Apps/Clock/ClockSrc/UI/MainScreen.py
@@ -7,6 +7,7 @@ from ESApi.AppScreen import AppScreen
 class MainScreen(AppScreen, BoxLayout):
     __background = None  # type: Widget
     __time = None  # type: Label
+    __date = None  # type: Label
 
 
     def __init__(self, **kwargs):
@@ -17,11 +18,17 @@ class MainScreen(AppScreen, BoxLayout):
 
         self.__time = Label()
         self.__time.text = "12:00"
-        self.__time.font_size = 125
-        self.__time.pos = (337, 200)
+        self.__time.font_size = 200
+        self.__time.pos = (337, 225)
         self.__time.color = [0, 0, 0, 1]
         self.__background.add_widget(self.__time)
 
+        self.__date = Label()
+        self.__date.text = "Sat 13.02.2016"
+        self.__date.font_size = 50
+        self.__date.pos = (337, 100)
+        self.__date.color = [0, 0, 0, 1]
+        self.__background.add_widget(self.__date)
 
     def _on_resize(self):
         pass
