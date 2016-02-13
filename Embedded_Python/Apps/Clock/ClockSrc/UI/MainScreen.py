7198d5a72e4b402106d354ae0ce618b1d4a61e9d
Sebastian Bischlager <sebastoan-bischlager@web.de>
2016-02-13 18:23:07 +0100

    Added test label to clock screen

--- Embedded_Python/Apps/Clock/ClockSrc/UI/MainScreen.py
+++ Embedded_Python/Apps/Clock/ClockSrc/UI/MainScreen.py
@@ -1,16 +1,27 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
+from kivy.uix.label import Label
 from ESApi.AppScreen import AppScreen
 
 
 class MainScreen(AppScreen, BoxLayout):
-    __testWidget = None  # type: Widget
+    __background = None  # type: Widget
+    __time = None  # type: Label
+
 
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-        self.__testWidget = Image()
-        self.__testWidget.color = [0, 1, 1, 1]
-        self.add_widget(self.__testWidget)
+        self.__background = Image()
+        self.__background.color = [1, 1, 1, 1]
+        self.add_widget(self.__background)
+
+        self.__time = Label()
+        self.__time.text = "12:00"
+        self.__time.font_size = 125
+        self.__time.pos = (337, 200)
+        self.__time.color = [0, 0, 0, 1]
+        self.__background.add_widget(self.__time)
+
 
     def _on_resize(self):
         pass
