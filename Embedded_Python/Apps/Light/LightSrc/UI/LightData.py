d87f68dc2ac41e5718353a8bcb1eb735bb7ed990
Sebastian Bischlager <sebastoan-bischlager@web.de>
2016-02-27 15:44:55 +0100

    Added brightness bar

--- Embedded_Python/Apps/Light/LightSrc/UI/LightData.py
+++ Embedded_Python/Apps/Light/LightSrc/UI/LightData.py
@@ -16,6 +16,7 @@ class LightData(AppScreen, BoxLayout):
     __btnBack = None  # type: Button
 
     __colorPalette = None  # type: Image
+    __brightnessBar = None  # type: Image
 
     def __init__(self, backCallback: callable, **kwargs):
         super(LightData, self).__init__(**kwargs)
@@ -26,10 +27,19 @@ class LightData(AppScreen, BoxLayout):
         self.__btnBack.text = "Back"
         self.__btnBack.bind(on_press=lambda instance: backCallback)
 
+        coreColorPalette = CoreImage.load(ESApi.FileIO.apps_directory() + "/Light/Media/HSBPalette.png")
         self.__colorPalette = Image()
+        self.__colorPalette.texture = coreColorPalette.texture
         self.__colorPalette.allow_stretch = True
         self.__colorPalette.keep_ratio = False
         self.__background.add_widget(self.__colorPalette)
+
+        coreBrightnessBar = CoreImage.load(ESApi.FileIO.apps_directory() + "/Light/Media/BrightnessBar.png");
+        self.__brightnessBar = Image()
+        self.__brightnessBar.texture = coreBrightnessBar.texture
+        self.__brightnessBar.allow_stretch = True
+        self.__brightnessBar.keep_ratio = False
+        self.__background.add_widget(self.__brightnessBar)
 
     def get_topbar_buttons(self):
         return [self.__btnBack]
@@ -40,14 +50,13 @@ class LightData(AppScreen, BoxLayout):
     def on_touch_move(self, touch):
         self.__touch_input(touch)
 
-    def on_set(self):
-        coreColorPalette = CoreImage.load(ESApi.FileIO.apps_directory() + "/Light/Media/HSBPalette.png")
-        self.__colorPalette.texture = coreColorPalette.texture
-
     def _on_resize(self):
         size = self.size[1]
         self.__colorPalette.size = (size, size)
         self.__colorPalette.pos = (self.size[0] - size, 50)
+
+        self.__brightnessBar.size = (50, size)
+        self.__brightnessBar.pos = (self.__colorPalette.pos[0], 50)
 
     def __touch_input(self, touch):
         if self.__colorPalette.collide_point(touch.pos[0], touch.pos[1]): # Color Palette Input
