1f5a81f967c42af2c2abe7f0b0235463d87c7172
Sebastian Bischlager <sebastoan-bischlager@web.de>
2016-02-27 14:13:06 +0100

    Work on lights

--- Embedded_Python/Apps/Light/LightSrc/UI/LightData.py
+++ Embedded_Python/Apps/Light/LightSrc/UI/LightData.py
@@ -1,25 +1,35 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
+from kivy.core.image import Image as CoreImage
 from kivy.uix.button import Button
 from kivy.core.window import Window
+from kivy.graphics import Color
 
 from ESApi.AppScreen import AppScreen
 
+import ESApi.FileIO
 
 class LightData(AppScreen, BoxLayout):
     __background = None  # type: Image
 
     __btnBack = None  # type: Button
 
+    __colorPalette = None  # type: Image
+
     def __init__(self, backCallback: callable, **kwargs):
         super(LightData, self).__init__(**kwargs)
-        self.__background = Image(color=[0.3, 0.7, 0.5, 1])
+        self.__background = Image(color=[1, 1, 1, 1])
         self.add_widget(self.__background)
 
         self.__btnBack = Button()
         self.__btnBack.text = "Back"
         self.__btnBack.bind(on_press=lambda instance: backCallback)
+
+        self.__colorPalette = Image()
+        self.__colorPalette.allow_stretch = True
+        self.__colorPalette.keep_ratio = False
+        self.__background.add_widget(self.__colorPalette)
 
     def get_topbar_buttons(self):
         return [self.__btnBack]
@@ -30,5 +40,28 @@ class LightData(AppScreen, BoxLayout):
     def on_touch_move(self, touch):
         self.__touch_input(touch)
 
+    def on_set(self):
+        coreColorPalette = CoreImage.load(ESApi.FileIO.apps_directory() + "/Light/Media/HSBPalette.png")
+        self.__colorPalette.texture = coreColorPalette.texture
+
+    def _on_resize(self):
+        size = self.size[1]
+        self.__colorPalette.size = (size, size)
+        self.__colorPalette.pos = (self.size[0] - size, 50)
+
     def __touch_input(self, touch):
-        print("Touched: " + str(touch.pos))
+        if self.__colorPalette.collide_point(touch.pos[0], touch.pos[1]): # Color Palette Input
+            frameX = self.__colorPalette.pos[0]
+            frameY = self.__colorPalette.pos[1]
+            frameW = self.__colorPalette.size[0]
+            frameH = self.__colorPalette.size[1]
+            touchX = touch.pos[0]
+            touchY = touch.pos[1]
+
+            relativeX = float(touchX - frameX) / float(frameW)
+            relativeY = float(touchY - frameY) / float(frameH)
+
+            sat = int(relativeX * 255)
+            hue = int(65535 - (relativeY * 65535))
+
+            self.__background.color = Color(float(hue) / float(65535), relativeX, 1, mode="hsv").rgba
