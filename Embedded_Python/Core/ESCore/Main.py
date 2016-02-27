31be5b5fa3269d4a87412436e948ca4a7ab3d83b
Sebastian Bischlager <sebastoan-bischlager@web.de>
2016-02-27 14:54:33 +0100

    Debugging boot loop

--- Embedded_Python/Core/ESCore/Main.py
+++ Embedded_Python/Core/ESCore/Main.py
@@ -12,10 +12,6 @@ Config.set('graphics', 'height', '480')
 sys.path.append(CoreFileIO.core_directory())
 sys.path.append(CoreFileIO.core_directory() + "/ESCore/pydev")
 
-import pydevd
-
-pydevd.settrace('192.168.178.47', port=46645, stdoutToServer=True, stderrToServer=True)
-
 from ESCore.UI.KivyMain import KivyMain
 
 CoreFileIO.create_directories()
